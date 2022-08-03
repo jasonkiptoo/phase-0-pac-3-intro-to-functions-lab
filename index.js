8f89cb30d553f50b2486ca1c8b272e74569b8975
jasonkiptoo <kiptoojason@gmail.com>
2022-08-03 09:59:36 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,44 @@
+function shout(string){
+const name="hello"
+ 
+return name.toUpperCase();
+}
+
+function whisper(){
+    const name="HELLO"
+    return name.toLowerCase();
+}
+
+function logShout(string){
+    const name="HELLO"
+    
+   console.log(name.toUpperCase())
+}
+
+function logWhisper(string){
+    let name="hello"
+     
+// name=(console,name.toUpperCase())
+console.log(name.toLowerCase())
+}
+
+
+function sayHiToHeadphonedRoommate(string){
+
+
+    const sayHi="I can\'t hear you!";
+    const sayYes="YES INDEED!";
+    const iWould="I would love to!";
+
+if(string.toLowerCase(string)===string){
+    return sayHi;
+}
+else if(string.toUpperCase(string)===string){
+   return sayYes
+
+}
+else if(iWould===iWould){
+    return iWould
+}
+
+}
